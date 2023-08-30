2dd6372656b9d7514eeb69d83585010a414960a9
AndreyV11 <127048647+AndreyV11@users.noreply.github.com>
2023-08-30 21:04:35 +0300

    [upd] add photo command

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ async def on_startup(_):
     print("Бот был успешно запущен")
 
 
+# функция приветствия, отправка стикера, message.from_user.id -> отправляет сообщение в чат пользователю
 @dp.message_handler(commands=['start'])
 async def start_command(message: types.Message):
     await message.answer(text="<em>Добро пожаловать на наш Телеграмм-канал!</em>", parse_mode="HTML")
@@ -17,16 +18,29 @@ async def start_command(message: types.Message):
     await message.delete()
 
 
+# функция help, список команд, parse_mode - разметка сообщения
 @dp.message_handler(commands=['help'])
 async def help_command(message: types.Message):
-    await message.answer(text=HELP_INFO, parse_mode="HTML")
+    await bot.send_message(text=HELP_INFO, parse_mode="HTML",
+                           chat_id=message.from_user.id)
+    await message.delete()
 
 
+# описание бота, message.answer отвечает туда, откуда пришло сообщение
 @dp.message_handler(commands=['description'])
 async def description_command(message: types.Message):
     await message.answer(text=DESCRIPTION_INFO)
 
 
+# отправка изображения, доступно как по интернет-ссылке, так и из локальной памяти, message.chat.id - в групповой чат
+@dp.message_handler(commands=["photo"])
+async def photo_command(message: types.Message):
+    await bot.send_photo(chat_id=message.chat.id,
+                         photo="https://i.pinimg.com/originals/1e/b3/78/1eb3786697979899fadbd770f05ba9d5.png")
+    await message.delete()
+
+
+# отправка сообщений в зависимомти от сообщения пользователя
 @dp.message_handler()
 async def hello_echo_command(message: types.Message):
     if message.text.lower() in HELLO_USER:
@@ -39,5 +53,14 @@ async def hello_echo_command(message: types.Message):
         await message.reply(text="❤️")
 
 
+## отправка геолокации
+#@dp.message_handler(commands=["location"])
+#async def location_command(message: types.Message):
+#    await bot.send_location(chat_id=message.from_user.id, longitude=55, latitude=18)
+#    await message.delete()
+
+
 if __name__ == '__main__':
-    executor.start_polling(dp, on_startup=on_startup)
+    executor.start_polling(dp, on_startup=on_startup, skip_updates=True)
+    # skip_updates=True -> бот не будет отвечать на сообщения, полученные оффлайн
+    # on_startup - функция, вызывающаяся при запуске бота
