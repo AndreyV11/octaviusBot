82141cd3422930c9b31d8e8cd2744f60b7e00faf
AndreyV11 <127048647+AndreyV11@users.noreply.github.com>
2023-08-30 19:11:49 +0300

    [upd] add help, description and hello command

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from aiogram import Bot, Dispatcher, executor, types
-from config import TOKEN_API
-
+from config import TOKEN_API, HELP_INFO, DESCRIPTION_INFO, HELLO_USER
 
 bot = Bot(TOKEN_API)
 dp = Dispatcher(bot)
@@ -10,6 +9,22 @@ dp = Dispatcher(bot)
 async def start_command(message: types.Message):
     await message.answer(text="Добро пожаловать на наш Телеграмм-канал!")
     await message.delete()
+
+
+@dp.message_handler(commands=['help'])
+async def help_command(message: types.Message):
+    await message.answer(text=HELP_INFO)
+
+
+@dp.message_handler(commands=['description'])
+async def description_command(message: types.Message):
+    await message.answer(text=DESCRIPTION_INFO)
+
+
+@dp.message_handler()
+async def hello_echo_command(message: types.Message):
+    if message.text.lower() in HELLO_USER:
+        await message.answer(text=message.text)
 
 
 if __name__ == '__main__':
