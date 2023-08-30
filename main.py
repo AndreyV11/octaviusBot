001f7515af8a8c65f64984a14347b66d31369c5c
AndreyV11 <127048647+AndreyV11@users.noreply.github.com>
2023-08-30 21:39:53 +0300

    [upd] add keyboard

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton
 from config import TOKEN_API, HELP_INFO, DESCRIPTION_INFO, HELLO_USER
+
 
 bot = Bot(TOKEN_API)
 dp = Dispatcher(bot)
 
+# клавиатура, resize_keyboard изменяет размер на оптимальный, one_time_keyboard сворачивает клавиатура после нажатия
+kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
+btn1 = KeyboardButton('/help')
+btn2 = KeyboardButton('/description')
+btn3 = KeyboardButton('/photo')
+kb.add(btn1).insert(btn2).add(btn3)     # add - добавить в строку, insert - добавить в столбец
+
 
 async def on_startup(_):
-    print("Бот был успешно запущен")
+    print("Start is successfully")
 
 
 # функция приветствия, отправка стикера, message.from_user.id -> отправляет сообщение в чат пользователю
 @dp.message_handler(commands=['start'])
 async def start_command(message: types.Message):
-    await message.answer(text="<em>Добро пожаловать на наш Телеграмм-канал!</em>", parse_mode="HTML")
-    await bot.send_sticker(message.from_user.id,
-                           sticker="CAACAgIAAxkBAAEKLEpk73VCGsVsdN5T3S_FqvBtg6PSpwACtgkAAnlc4gnGTnKNypclSDAE")
+    await bot.send_message(chat_id=message.from_user.id,
+                           text="<em>Добро пожаловать на наш Телеграмм-канал!</em>",
+                           parse_mode="HTML")
+    await bot.send_sticker(chat_id=message.from_user.id,
+                           sticker="CAACAgIAAxkBAAEKLEpk73VCGsVsdN5T3S_FqvBtg6PSpwACtgkAAnlc4gnGTnKNypclSDAE",
+                           reply_markup=kb)
     await message.delete()
 
 
@@ -22,7 +34,8 @@ async def start_command(message: types.Message):
 @dp.message_handler(commands=['help'])
 async def help_command(message: types.Message):
     await bot.send_message(text=HELP_INFO, parse_mode="HTML",
-                           chat_id=message.from_user.id)
+                           chat_id=message.from_user.id,
+                           reply_markup=ReplyKeyboardRemove())      # ReplyKeyboardRemove полностью убирает клавиатуру
     await message.delete()
 
 
